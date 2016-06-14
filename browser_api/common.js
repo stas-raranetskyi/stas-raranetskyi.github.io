5dbfb75eecd0357365addd8e9c55b61f9829545c
Stas Raranetsky <r.starta@yandex.ua>
2016-06-15 01:18:55 +0300

    +

--- browser_api/common.js
+++ browser_api/common.js
@@ -1,15 +1,16 @@
 (function() {
-  var video = document.getElementById('video'),
-   vendorUrl = window.URL || window.webkitURL;
-  navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.
-mozGetUserMedia || navigator.msGetUserMedia;
-  navigator.getMedia({
-   video: true,
-   audio: true
-  }, function(stream) {
-   video.src = vendorUrl.createObjectURL(stream);
-   video.play();
-  }, function(error) {
-   console.log('Ошибка! Что-то пошло не так, попробуйте позже.');
-  });
+	var video = document.getElementById('video'),
+  	  vendorUrl = window.URL || window.webkitURL;
+  	  navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+  
+	navigator.getMedia({
+		video: true,
+		audio: true
+	}, function(stream) {
+		video.src = vendorUrl.createObjectURL(stream);
+		video.play();
+	}, function(error) {
+		console.log('Ошибка! Что-то пошло не так, попробуйте позже.');
+	});
+
 })();
