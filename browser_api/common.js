ca640a714b55ecea605be85498e612d6cad893c8
Stas Raranetsky <r.starta@yandex.ua>
2016-06-19 00:44:10 +0300

    +

--- browser_api/common.js
+++ browser_api/common.js
@@ -1,20 +1,28 @@
 (function() {
-	var video = document.getElementById('video'),
-  	  vendorUrl = window.URL || window.webkitURL;
-  	  navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-  	  console.log(navigator.getUserMedia);
-  	  console.log(navigator.webkitGetUserMedia);
-  	  console.log(navigator.mozGetUserMedia);
-  	  console.log(navigator.msGetUserMedia);
-  
-	navigator.getMedia({
-		video: true,
-		audio: false
-	}, function(stream) {
-		video.src = vendorUrl.createObjectURL(stream);
-		video.play();
-	}, function(error) {
-		console.log('Ошибка! Что-то пошло не так, попробуйте позже.');
-	});
+    var video = document.getElementById('video'),
+    vendorUrl = window.URL || window.webkitURL;
+    navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    console.log(navigator.getUserMedia);
+    console.log(navigator.webkitGetUserMedia);
+    console.log(navigator.mozGetUserMedia);
+    console.log(navigator.msGetUserMedia);
+
+
+    navigator.getMedia({
+        video: true,
+        audio: false
+    }, function(stream) {
+        video.src = vendorUrl.createObjectURL(stream);
+        video.play();
+    }, function(error) {
+        console.log('Ошибка! Что-то пошло не так, попробуйте позже.');
+    });
+
+    navigator.geolocation.getCurrentPosition(showPosition); // Запрашиваем местоположение, и в случае успеха вызываем функцию showPosition
+    function showPosition(position) {
+        /* Выводим координаты */
+        document.write("Широта: " + position.coords.latitude + "<br />");
+        document.write("Долгота: " + position.coords.longitude);
+    }
 
 })();
