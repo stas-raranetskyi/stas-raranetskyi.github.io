bdc7cd1584e26c8a7292d0e794c3b4254921b2b7
Stas Raranetsky <r.starta@yandex.ua>
2016-06-19 23:19:49 +0300

    +

--- browser_api/common.js
+++ browser_api/common.js
@@ -6,7 +6,9 @@
     var video = document.getElementById('video'),
     vendorUrl = window.URL || window.webkitURL;
     navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia,
-    button = document.getElementById('button');
+    button = document.getElementById('button'),
+    canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d');
 
     navigator.getMedia({
         video: true,
@@ -17,6 +19,26 @@
     }, function(error) {
         console.log('Ошибка! Что-то пошло не так, попробуйте позже.');
     });
+
+     // функция которая будет выполнена при нажатии на кнопку захвата кадра
+    var captureMe = function () {
+      if (!videoStreamUrl) alert('То-ли вы не нажали "разрешить" в верху окна, то-ли что-то не так с вашим видео стримом')
+      // переворачиваем canvas зеркально по горизонтали (см. описание внизу статьи)
+      context.translate(canvas.width, 0);
+      context.scale(-1, 1);
+      // отрисовываем на канвасе текущий кадр видео
+      context.drawImage(video, 0, 0, video.width, video.height);
+      // получаем data: url изображения c canvas
+      var base64dataUrl = canvas.toDataURL('image/png');
+      context.setTransform(1, 0, 0, 1, 0, 0); // убираем все кастомные трансформации canvas
+      // на этом этапе можно спокойно отправить  base64dataUrl на сервер и сохранить его там как файл (ну или типа того) 
+      // но мы добавим эти тестовые снимки в наш пример:
+      var img = new Image();
+      img.src = base64dataUrl;
+      window.document.body.appendChild(img);
+    }
+
+    button.addEventListener('click', captureMe);
     
 
 
