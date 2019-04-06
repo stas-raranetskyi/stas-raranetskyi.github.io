d4686e0784d58a8f0be6a9f6faeea23f0ff1b629
Stas R <rar.stas@gmail.com>
2019-04-06 12:14:19 +0300

    clean code

--- snake/js/main.js
+++ snake/js/main.js
@@ -11,104 +11,102 @@ Object.prototype.copy = function() {
 };
 
 var Game = function(name){
-
-    /*fields*/
-    this.canvas = document.getElementById(name);
-    this.context = this.canvas.getContext('2d');
-    this.gameEngine;
-    this.n = 30;
-    this.m = 20;
-    this.scale = 30;
-    this.width = this.n * this.scale;
-    this.height = this.m * this.scale;
-    this.canvas.width = this.width;
-    this.canvas.height = this.height;
-    this.snake = [{x: 0, y: 0}];
-    this.xdir = 1;
-    this.ydir = 0;
-    var game = this;
+    var canvas = document.getElementById(name);
+    var context = canvas.getContext('2d');
+    var gameEngine;
+    var n = 30;
+    var m = 20;
+    var scale = 30;
+    var width = n * scale;
+    var height = m * scale;
+    var xdir = 1;
+    var ydir = 0;
+    var snake = [{x: 0, y: 0}];
+    var food = setPosFood();
     var pause = false;
     var fpsInterval;
     var then;
     var elapsed;
     var now;
     var loopId = null;
+    canvas.width = width;
+    canvas.height = height;
 
-    this.getHead = function(){
-        return game.snake[game.snake.length - 1].copy();
+    function getHead(){
+        return snake[snake.length - 1].copy();
     }
 
-    this.updateSnake = function(){
-        var head = this.getHead();
-        game.snake.shift();
-        head.x += this.xdir;
-        head.y += this.ydir;
-        game.snake.push(head);
+    function updateSnake(){
+        var head = getHead();
+        snake.shift();
+        head.x += xdir;
+        head.y += ydir;
+        snake.push(head);
     }
 
-    this.growSnake = function(){
-        var head = this.getHead();
-        game.snake.push(head);
+    function growSnake(){
+        var head = getHead();
+        snake.push(head);
     }
 
-    this.drawSnake = function(){
-        game.context.fillStyle = '#000000';
-        for(var i = 0; i < game.snake.length; i++){
-            game.context.fillRect(game.snake[i].x * this.scale, game.snake[i].y * this.scale, this.scale, this.scale);
+    function drawSnake(){
+        context.fillStyle = '#000000';
+        for(var i = 0; i < snake.length; i++){
+            context.fillRect(snake[i].x * scale, snake[i].y * scale, scale, scale);
         }
     }
 
-    this.drawApple = function(){
-        game.context.fillStyle = '#ff0000';
-        game.context.fillRect(game.apple.x * this.scale, game.apple.y * this.scale, this.scale, this.scale);
+    function drawFood(){
+        context.fillStyle = '#ff0000';
+        context.fillRect(food.x * scale, food.y * scale, scale, scale);
     };
 
-    this.eat = function(food){
-        var head = this.getHead();
+    function eat(food){
+        var head = getHead();
         return head.x === food.x && head.y === food.y;
     }
 
-    this.setPosFood = function(){
-        var x = Math.floor(Math.random() * this.n) || 0;
-        var y = Math.floor(Math.random() * this.m) || 0;
+    function setPosFood(){
+        var x = Math.floor(Math.random() * n) || 0;
+        var y = Math.floor(Math.random() * m) || 0;
         return {x: x, y: y}
     }
 
-    this.endGame = function() {
-        var head = this.getHead();
-        var x = head.x * this.scale;
-        var y = head.y * this.scale;
-        if(x > this.width - 1 || x < 0 || y > this.height - 1 || y < 0) {
+    function endGame() {
+        var head = getHead();
+        var x = head.x * scale;
+        var y = head.y * scale;
+        if(x > width - 1 || x < 0 || y > height - 1 || y < 0) {
             return true;
         }
-        for(var i = 0; i < this.snake.length - 1; i++) {
-            var part = this.snake[i];
-            if(part.x * this.scale == x && part.y * this.scale == y) {
+        for(var i = 0; i < snake.length - 1; i++) {
+            var part = snake[i];
+            if(part.x * scale == x && part.y * scale == y) {
                 return true;
             }
         }
         return false;
     }
 
-    this.drawGame = function(){
-        game.context.clearRect(0, 0, game.width, game.height);
-        this.updateSnake();
-        this.drawSnake();
-        if(this.endGame()){
+    function drawGame(){
+        context.clearRect(0, 0, width, height);
+        updateSnake();
+        drawSnake();
+        if(endGame()){
             stop(loopId);
-            game.context.fillStyle = '#ff0000';
-            game.context.fillRect(0, 0, this.width, this.width);
+            context.fillStyle = '#ff0000';
+            context.fillRect(0, 0, width, width);
         }
-        if(this.eat(game.apple)){
-            this.growSnake();
-            this.apple = this.setPosFood();
+        if(eat(food)){
+            growSnake();
+            food = setPosFood();
         }
-        this.drawApple();
+        drawFood();
     };
 
-    this.setDir = function(x, y){
-        game.xdir = x;
-        game.ydir = y;
+    function setDir(x, y){
+        xdir = x;
+        ydir = y;
     }
 
     var loop = (function(){
@@ -134,17 +132,17 @@ var Game = function(name){
     })();
 
     document.onkeydown = function (event) {
-        if(event.keyCode == 37 && game.xdir != 1){
-            game.setDir(-1, 0);
+        if(event.keyCode == 37 && xdir != 1){
+            setDir(-1, 0);
         }
-        else if(event.keyCode == 38 && game.ydir != 1){
-            game.setDir(0, -1);
+        else if(event.keyCode == 38 && ydir != 1){
+            setDir(0, -1);
         }
-        else if(event.keyCode == 39 && game.xdir != -1){
-            game.setDir(1, 0);
+        else if(event.keyCode == 39 && xdir != -1){
+            setDir(1, 0);
         }
-        else if(event.keyCode == 40 && game.ydir != -1){
-            game.setDir(0, 1);
+        else if(event.keyCode == 40 && ydir != -1){
+            setDir(0, 1);
         }
         else if(event.keyCode == 27 || event.keyCode == 32){
             pause = true;
@@ -152,38 +150,35 @@ var Game = function(name){
         }
         else if(event.keyCode == 13){
             if(pause){
-                game.gameEngineStep();
+                gameEngineStep();
                 pause = false;
             }
         }
     };
 
-    this.gameEngineStart = function(fps, callback){
-        if(typeof callback == 'undefined'){
-            callback = game.drawGame;
-        }
-        if(typeof fps == 'undefined'){
-            fps = 7;
-            fpsInterval = 1000 / fps;
-            then = Date.now();
-        }
-        game.gameEngine = callback;
-        this.apple = this.setPosFood();
-        game.gameEngineStep();
-    }
-
-    this.gameEngineStep = function(){
-        loopId = loop(game.gameEngineStep);
+    function gameEngineStep(){
+        loopId = loop(gameEngineStep);
 		now = Date.now();
         elapsed = now - then;
         if (elapsed > fpsInterval) {
             then = now - (elapsed % fpsInterval);
-            game.gameEngine();
+            gameEngine();
         }
     };
+
+    this.gameEngineStart = function(fps, callback){
+        if(typeof callback == 'undefined'){
+            callback = drawGame;
+        }
+        if(typeof fps == 'undefined'){
+            fps = 7;
+        }
+        fpsInterval = 1000 / fps;
+        then = Date.now();
+        gameEngine = callback;
+        gameEngineStep();
+    }
 }
 
 var game = new Game('game');
-
-
 game.gameEngineStart();
